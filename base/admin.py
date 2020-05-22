61f0fbc88e1f70edd8a51bcf9412eb5cdb0ab3f1
Henrique Mariano <henrique.marino25@hotmail.com>
2020-05-21 21:56:24 -0300

    Add migrations, Employee, Sector, SubSector

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Sector, SubSector, Employee
 
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(SubSector)
+admin.site.register(Sector)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+class SubSector(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Sector(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    cpf = models.IntegerField()
+    admission_date = models.DateField()
+    demission_date = models.DateField()
+    leader = models.BooleanField()
+    leader_name = models.CharField(max_length=100)
+    manager = models.CharField(max_length=100)
+    registration = models.IntegerField()
+    occupation = models.CharField(max_length=100)
+    extra_hour = models.FloatField()
+    sector = models.ForeignKey(Sector, on_delete=models.PROTECT)
+    sub_sector = models.ForeignKey(SubSector, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
