4d0bae2204128d81dc62cbd6d9bbf4bdb45305fd
Henrique Mariano <henrique.marino25@hotmail.com>
2020-05-28 21:35:43 -0300

    Ref #5 Fix import of dismissed employees

--- imports/views.py
+++ imports/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
@@ -26,47 +28,57 @@ def employees(request):
 
 def update_employees(request):
     if request.method == 'POST':
-        print(request.POST)
         excel = request.FILES['excel']
         datas = pd.read_excel(excel.file)
         for data in datas.index:
-            demission_date = datas['Data de demissao (DD/MM/AAAA)'][data]
-            if not demission_date == " ":
-                continue
-
             registration = str(datas['Matricula'][data])[-8:]
+            demission_date = datas['Data de demissao (DD/MM/AAAA)'][data]
             name = datas['Funcionario'][data]
             leader_name = datas['Lider equipe'][data]
             admission_date = datas['Data de admissao (DD/MM/AAAA)'][data]
             occupation = datas['Descricao funcao'][data]
             manager = datas['Nome gestor'][data]
+            if type(demission_date) == str:
+                demission_date = demission_date.strip()
 
-            subsector = str(datas['Empresa'][data]).split('-')[0]
-            print(SubSector.objects.filter(name=subsector).exists())
-            # print(SubSector.objects.filter(name=subsector))
-            if SubSector.objects.filter(name=subsector).exists():
-                subSector = SubSector.objects.filter(name=subsector).first()
-            else:
-                subSector = SubSector(name=subsector)
-                subSector.save()
+            if type(demission_date) == float:
+                demission_date = ""
 
             if registration != " ":
                 employee = Employee.objects.filter(registration=registration).first()
+                if not employee is None and demission_date != "":
+                    employee.delete()
+                    continue
+
+                if not demission_date == "":
+                    continue
+
+                subsector = str(datas['Empresa'][data]).split('-')[0]  # print(SubSector.objects.filter(name=subsector))
+                if SubSector.objects.filter(name=subsector).exists():
+                    subSector = SubSector.objects.filter(name=subsector).first()
+                else:
+                    subSector = SubSector(name=subsector)
+                    subSector.save()
+
                 if not employee == None:
                     if not employee.name == name and employee.occupation == occupation and \
-                            employee.admission_date == admission_date and employee.manager == manager and \
-                            employee.leader_name == leader_name and employee.sub_sector == subSector:
-                        employee.name = name
-                        employee.occupation = occupation
-                        employee.leader_name = leader_name
-                        employee.admission_date = admission_date
-                        employee.manager = manager
-                        employee.sub_sector = subSector
+                                employee.admission_date == admission_date and employee.manager == manager and \
+                                employee.leader_name == leader_name and employee.sub_sector == subSector:
+                            employee.name = name
+                            employee.occupation = occupation
+                            employee.leader_name = leader_name
+                            employee.admission_date = admission_date
+                            employee.manager = manager
+                            employee.sub_sector = subSector
+
                 else:
                     employee = Employee(name=name, occupation=occupation, registration=registration,
                                         admission_date=admission_date, manager=manager,
                                         leader_name=leader_name, sub_sector=subSector)
                     employee.save()
+        data = {'employees': Employee.objects.all().order_by('name'),
+                'sectors': Sector.objects.all()}
+        return render(request, 'employees.html', data)
 
 
 def update_employees_leader(request):
