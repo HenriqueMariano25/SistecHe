fc8b39614cd9a1fc9bf0b576fb4d6e38212e7ea3
Henrique Mariano <henrique.marino25@hotmail.com>
2020-06-11 08:01:25 -0300

    Ref #41 Correction of report by shift to be able to select by shift

--- report/views.py
+++ report/views.py
@@ -3,77 +3,36 @@ from django.shortcuts import render
 from base.models import Shift, Employee, Emplo_Schedu, Scheduling, Sector
 
 
-# Create your views here.
 def report_shift(request):
-    global shifts_res
     shifts = Shift.objects.all()
     data = {'shifts': shifts}
-    # if request.method == "POST":
-    #     date = request.POST['date']
-    #     shift_params = request.POST['shift']
-    #     if shift_params != '0':
-    #         shifts_res = Shift.objects.get(id=int(shift_params))
-    #         emplo_schedus = Emplo_Schedu.objects.prefetch_related('employee', 'scheduling').filter(
-    #             scheduling__date=date,
-    #             scheduling__sector_id=int(
-    #                 shift_params))
-    #     else:
-    #         shifts_res = shifts
-    #         emplo_schedus = Emplo_Schedu.objects.prefetch_related('employee', 'scheduling').filter(
-    #             scheduling__date=date)
-    #
-    #     leaders = []
-    #     for emplo_schedu in emplo_schedus:
-    #         # scheduling = Scheduling.objects.get(id=emplo_schedu.)
-    #         leader = Employee.objects.get(name=emplo_schedu.employee.leader_name)
-    #         if not leader in leaders:
-    #             leaders.append(leader)
-    #
-    #
-    #
-    #     sectors = Sector.objects.all()
-    #
-    #     data = {'shifts': shifts, 'shifts_res': shifts_res, 'emplo_schedus': emplo_schedus, 'leaders':leaders, 'sectors':sectors}
-
     return render(request, 'report_shift.html', data)
 
 
 def shift_preview(request):
-    # print(request.GET)
     date = request.GET['date']
-    shift_params = request.GET['shift']
+    shift_params = int(request.GET['shift'])
 
-    if shift_params != '0':
-        shifts_res = Shift.objects.get(id=int(shift_params)).to_json()
+    if shift_params != 0:
+        shifts_res = Shift.objects.filter(id=int(shift_params))
         emplo_schedus = Emplo_Schedu.objects.prefetch_related('employee', 'scheduling').filter(
-            scheduling__date=date,
-            scheduling__sector_id=int(
-                shift_params))
+            scheduling__date=date,scheduling__shift_id=shift_params)
+        shifts_res = [shift.to_json() for shift in shifts_res]
     else:
         shifts_res_date = Shift.objects.all()
-        # print(shifts_res_date)
         shifts_res = [shift.to_json() for shift in shifts_res_date]
-        # for shift in shifts_res_date:
-        #     print(shift)
         emplo_schedus = Emplo_Schedu.objects.prefetch_related('employee', 'scheduling').filter(
             scheduling__date=date)
-    #
+
     leaders = []
     for emplo_schedu in emplo_schedus:
-        # scheduling = Scheduling.objects.get(id=emplo_schedu.)
         leader = Employee.objects.get(name=emplo_schedu.employee.leader_name).to_json()
-        # print(leader)
         if not leader in leaders:
             leaders.append(leader)
 
     sectors = [sector.to_json() for sector in Sector.objects.all()]
 
     emplo_schedus_data = [emplo_schedu.to_json() for emplo_schedu in emplo_schedus]
-    print(emplo_schedus_data)
-    # print(shifts_res)
-    # print(leaders)
-    # print(sectors)
-    print(date)
     date_split = date.split('-')
     formatted_date = date_split[2] + "/" + date_split[1] + "/" +date_split[0]
     response = {
