c3d1306fbc2115792fe122040f65a1574f5a8aa9
Henrique Mariano <henrique.mariano25@hotmail.com>
2020-08-14 10:50:13 -0300

    Fix error report shift

--- report/views.py
+++ report/views.py
@@ -75,7 +75,7 @@ def shift_pdf(request):
     shifts = []
     sectors = []
     for shift in shifts_res:
-        if emplo_schedus.filter(scheduling__shift=shift):
+        if emplo_schedus.filter(scheduling__shift=shift).select_related():
             shifts.append(shift)
 
     for emplo_schedu in emplo_schedus:
@@ -87,7 +87,7 @@ def shift_pdf(request):
             continue
 
     for leader in leaders:
-        sector = Sector.objects.get(id=leader.sector_id)
+        sector = Sector.objects.get(id=leader.sector_id).select_related()
         if not sector in sectors:
             sectors.append(sector)
 
