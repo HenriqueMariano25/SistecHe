ca86f7bb44bf3fe6d74f81a41d77ff931619a32b
Henrique Mariano <henrique.mariano@hotmail.com>
2020-09-24 11:51:35 -0300

    Corrigi erro do PDF geral 3

--- report/views.py
+++ report/views.py
@@ -32,7 +32,7 @@ def shift_preview(request):
 
     leaders = []
     for emplo_schedu in emplo_schedus:
-        leader = Employee.objects.filter(name=emplo_schedu.employee.leader_name).first.to_json()
+        leader = Employee.objects.get(name=emplo_schedu.employee.leader_name).to_json()
         if not leader in leaders:
             leaders.append(leader)
 
@@ -186,7 +186,7 @@ def leader_preview(request):
 
     leaders = []
     for emplo_schedu in emplo_schedus:
-        leader = Employee.objects.filter(name=emplo_schedu.employee.leader_name).first.to_json()
+        leader = Employee.objects.get(name=emplo_schedu.employee.leader_name).to_json()
         if not leader in leaders:
             leaders.append(leader)
 
